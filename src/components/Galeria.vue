3198dc16d6a9b46a3fb8155a9266480b315b9ca2
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-02-03 18:53:37 +0100

    Curso de Vue.js con el CLI 3. Crear e importar Componentes.

---
+++ src/components/Galeria.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="galeria">
+        <h1>{{mensaje}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+        mensaje: 'This is a gallery component'
+      }
+    }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Galeria></Galeria>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Galeria from '@/components/Galeria.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Galeria
   }
 }
 </script>
