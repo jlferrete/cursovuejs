2dedc2d95a3458be2f5db13fa723cfe13cc0c2e8
Jose Luis Ferrete <joseluisferrete@gmail.com>
2019-02-03 19:27:26 +0100

    Curso Vue.js con CLI 3. Crear Barra de Navegación.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
+    <barnav />
     <router-view/>
   </div>
 </template>
+
+<script>
+  import Barnav from '@/components/Barnav'
+  export default {
+    name: 'app',
+    components: {
+      Barnav
+    }
+  }
+</script>
+
 
 <style>
 
